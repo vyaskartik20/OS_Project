e9642af87b2397fade14563d1325274c7091c632
Kartik Vyas <vyas.2@iitj.ac.in>
2021-05-04 21:58:55 +0530

    socket finalised

--- transfer/socket/client.py
+++ transfer/socket/client.py
@@ -1,58 +1,49 @@
 import socket
-import os
-import time
 
-CLIENT_DIR = "clientFiles"
+CLIENT_DIR = "TransferredFiles/"
 
-#Establishing connection
 s = socket.socket()
-# host = input("Please enter the hostname of the server : ")
-port = 9080
-s.connect(("127.0.1.1", port))
+# host = socket.gethostname()
+host = input("Enter the IP of the host server : ")
+port = 9002
+s.connect((host, port))
 
-numFiles = s.recv(100).decode()
-numFiles = int(numFiles)
-s.send(str("temp").encode())
+password = input("Enter the password to initiate the process : ")
+s.send(password.encode())
 
-      
-print("File receiving initiated")
-#song transferring to this client from the server endpoint
-for i in range(numFiles):
-    file = s.recv(1024).decode()
-    print(f"Receiving file {file}")
-    s.send(str("temp").encode())
+passwordCheck = s.recv(128).decode()
 
+if(passwordCheck == "Y"):
 
-    # filename = os.path.join(CLIENT_DIR, file)
-    # f = open(filename, 'wb')
-    # file_data = s.recv(10241024)
-    # while 1:
-    #     try :
-    #         print(file_data.decode())
-    #         print()
-    #         print()
-    #         print()
-    #         print()
-    #         if(file_data.decode()) == "Single File Send Terminated !!":
-    #             print('yes')
-    #             break
-    #     except :
-    #         print('no')
-    #         f.write(file_data)
-    #         file_data = s.recv(1024102424)
-    
-    # f.close()
+    print("File receiving initiated")
 
+    while True:
+        size = s.recv(16).decode() # Note that you limit your filename length to 255 bytes.
+        if not size:
+            break
+        size = int(size, 2)
+        filename = s.recv(size).decode()
+        print(f"Receiving file {filename}")
+        filesize = s.recv(32).decode()
+        filesize = int(filesize, 2)
+        filename = CLIENT_DIR + filename
+        file_to_write = open(filename, 'wb')
+        chunksize = 4096
+        while filesize > 0:
+            if filesize < chunksize:
+                chunksize = filesize
+            data = s.recv(chunksize)
+            file_to_write.write(data)
+            filesize -= len(data)
 
-    filename = os.path.join(CLIENT_DIR, file)
-    f = open(filename, 'wb')
-    file_data = s.recv(1024102424)
-    f.write(file_data)    
-    f.close()
+        file_to_write.close()
+        print(f"Received file saved as {filename}")
 
-    s.send(str("temp").encode())
+    print ('All files received successfully')
+    s.close()
+
+else :
+    print("Wrong Password entered, kindly try again !!! ")
 
 
-    print(f"The file {file} received")
-
-print("Receiving all the required files")
+print()
--- transfer/socket/server.py
+++ transfer/socket/server.py
@@ -1,64 +1,64 @@
 import socket
 import os
-import time
 
-#Establishing connection
-s = socket.socket()
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostbyname(socket.getfqdn())
-# host = socket.gethostname()
-port = 9080
-print(host)
-s.bind((host,port))
-s.listen()
-
-clientsocket, address = s.accept()
-print(f"Connected with {address}")
-
-listOfFiles = []
-listOfFiles.append("/home/vyas20/OS/lab3/B18CSE020/readme.txt")
-# listOfFiles.append("/home/vyas20/Pictures/1.png")
-listOfFiles.append("/home/vyas20/Pictures/3.png")
-listOfFiles.append("/home/vyas20/Pictures/4.png")
-listOfFiles.append("/home/vyas20/Pictures/2.png")
-listOfFiles.append("/home/vyas20/Downloads/bb.mkv")
+directory = []
+directory.append("/home/vyas20/OS/lab3/B18CSE020/readme.txt")
+directory.append("/home/vyas20/Pictures/1.png")
+directory.append("/home/vyas20/Pictures/3.png")
+directory.append("/home/vyas20/Pictures/4.png")
+directory.append("/home/vyas20/Pictures/2.png")
+# directory.append("/home/vyas20/Downloads/bb.mkv")
 
 
-print(f"Starting file transferring")
+serversock = socket.socket()
+host = socket.gethostbyname(socket.gethostname())
+print(f"Enter the address at the other machine : {host}")
+port = 9002
+serversock.bind((host,port))
+filename = ""
+serversock.listen(10)
+print ("Waiting for the connection.....")
 
-clientsocket.send((str(len(listOfFiles))).encode())
-temp = clientsocket.recv(1024).decode()
+clientsocket,addr = serversock.accept()
+print("Got a connection from %s" % str(addr))
+
+password = clientsocket.recv(100).decode()
+
+if(password == "project") :
+    print("Authentication with the other machine successful")
+
+    clientsocket.send(str("Y").encode())
+
+    print(f"Initiating file transferring")
+
+    for files in directory:
+        print(f"Transferring file {files}")
+
+        fileNames = files.split("/")
+        filename = fileNames[len(fileNames)-1]
+
+        size = len(filename)
+        size = bin(size)[2:].zfill(16) # encode filename size as 16 bit binary
+        clientsocket.send(size.encode())
+        clientsocket.send(filename.encode())
+
+        # filename = os.path.join(path,filename)
+        filesize = os.path.getsize(files)
+        filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
+        clientsocket.send(filesize.encode())
+        
+
+        file_to_send = open(files, 'rb')
+
+        l = file_to_send.read()
+        clientsocket.sendall(l)
+        file_to_send.close()
+        print(f"File {files} successfully transferred")
+
+else:
+    print("Authentication with the other machine failed")
+    clientsocket.send(str("N").encode())
 
 
-for file in listOfFiles :
-    if(os.path.isfile(file)):
-        fileNames = file.split("/")
-        fileName = fileNames[len(fileNames)-1]
-        print(f"Transferring file {file}")
-
-        clientsocket.send((fileName).encode())        
-        temp = clientsocket.recv(1024).decode()
-
-        # f = open(file , 'rb')
-        # file_data = f.read(10241024)
-        # while(file_data):
-        #     clientsocket.send(file_data)
-        #     file_data = f.read(10241024)
-        #     print(file_data)
-
-        # clientsocket.send((str("Single File Send Terminated !!")).encode())
-        # f.close()
-
-
-        f = open(file , 'rb')
-        file_data = f.read(1024102424)
-        clientsocket.send(file_data)
-        f.close()
-
-        temp = clientsocket.recv(1024).decode()
-
-
-
-        print(f"File {file} successfully transferred")
-
-print(f"File transferring completed !! ")
+serversock.close()
+print()
