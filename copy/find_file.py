b22be434693c037bcf32d372303a9435123492b8
garg-7 <garg.7@iitj.ac.in>
2021-05-05 23:22:18 +0530

    automatic window activation & constant title bar checking

--- copy/find_file.py
+++ copy/find_file.py
@@ -4,9 +4,13 @@ from signal import SIGINT
 from subprocess import PIPE, STDOUT, Popen, TimeoutExpired
 from threading import Thread
 from time import sleep
+import time
 
 from ewmh import EWMH
 
+class sharedInfo:
+    def __init__(self):
+        self.titles = []
 
 def get_children(pid):
     cmd = f"ps --ppid {pid}"
@@ -20,6 +24,42 @@ def sudo_kill(strace_pid):
     kill_proc = Popen(kill_cmd.split(" "), stderr=STDOUT, stdout=PIPE)
     kill_out, kill_err = kill_proc.communicate()
 
+
+def monitor_title_bar(window_manager: EWMH, pid: int, title_bar_info: sharedInfo):
+    open_windows = window_manager.getClientList()
+    # get window object from PID
+    curr_window = None
+    for w in open_windows:
+        if window_manager.getWmPid(w) == pid:
+            curr_window = w
+            break
+
+    if curr_window != None:
+        
+        # activate the window
+        window_manager.setActiveWindow(curr_window)
+
+        # run for 10 seconds, the while loop
+        # to accumulate title bars
+        titles = set()
+        init_time = time.time()
+        while time.time() - init_time < 10:
+            titles.add(window_manager.getWmName(curr_window).decode())
+    
+    else:
+        print("Well this shouldn't have happened.")
+        print(f"Something went wrong while trying to find the window with PID {pid}.")
+
+    if len(titles)==0:
+        print("I'm empty :(")
+        exit(1)
+    for k in titles:
+        title_bar_info.titles.append(k)
+    
+    return
+
+
+common_exes = ['mkv', 'mp4', 'mp3', 'flac', 'aac', 'wav', 'mov', ]
 
 # get open windows
 window_manager = EWMH()
@@ -58,6 +98,7 @@ for i in idx_to_transfer:
 # user_name = "devin"
 
 result = {}
+count = 0
 # get the open files for the PIDs
 for i1, (id, name) in enumerate(zip(pids_to_transfer, processes_to_transfer)):
     # first attach strace to the process
@@ -68,8 +109,15 @@ for i1, (id, name) in enumerate(zip(pids_to_transfer, processes_to_transfer)):
 
     # wait for some time
     print(f"Go to {name} and save your work")
-    sleep(10)
+    
+    # get title bars in the meantime
+    title_bar_info = sharedInfo()
+    t1 = Thread(target=monitor_title_bar, args=(window_manager, id, title_bar_info))
+    t1.start()
 
+    # sleep(10)
+
+    t1.join()
     # kill the trace
     # obtain the child of the strace_proc
     # because that is the one we want to kill
@@ -84,7 +132,7 @@ for i1, (id, name) in enumerate(zip(pids_to_transfer, processes_to_transfer)):
     # parse the trace
     out = out.decode()
 
-    file_found = []
+    files_found = []
     for line in out.strip().split("\n")[1:]:
         if "(" in line and ")" in line:
             line = line.split("(")[1].split(")")[0]
@@ -102,26 +150,38 @@ for i1, (id, name) in enumerate(zip(pids_to_transfer, processes_to_transfer)):
                     else:
                         # add to set
                         if os.path.isfile(p):
-                            if p not in file_found:
-                                file_found.append(p)
+                            if p not in files_found:
+                                files_found.append(p)
 
     # see if any file was found or should I run the title bar check?
-    if len(file_found) == 0:
-        tokens = title_bars[pids.index(id)].split()
-        for p in tokens:
-            if p.startswith("/home/"):
-                # add to set
-                if os.path.isfile(p):
-                    if p not in file_found:
-                        file_found.append(p)
+    if len(files_found) == 0:
+        # no files were found, get data from the title bar checks
+        for t in title_bar_info.titles:
+            # print(t)
+            tokens = t.split()
+            for p in tokens:
+                if p.startswith(("/home/", "~/")):
+                    # add to set
+                    if os.path.isfile(os.path.abspath(os.path.expanduser(p))):
+                        if p not in files_found:
+                            files_found.append(os.path.abspath(os.path.expanduser(p)))
 
-    print("Used files found: ")
-    for i, f in enumerate(file_found):
-        print(f"[{i}]: {f}")
+    if len(files_found)>0:
+        print("Files in use: ")
+        for i, f in enumerate(files_found):
+            print(f"[{i}]: {f}")
 
-    f_idx = input("Enter the index of the file to transfer: ")
-
-    result[i1] = {"process": name, "file": file_found[int(f_idx)]}
+        f_idx = input("Enter the indices of the file to transfer: ")
+        
+        for f in f_idx.strip().split():
+            result[count] = {"process": name, "file": files_found[int(f)]}
+            count+=1
+    else:
+        print(f"Couldn't find any open files for process: [{name}]")
+        file_alt = input("Enter a path instead (enter to skip)")
+        if not(file_alt == '-' or file_alt == ''):
+            result[count] = {"process": name, "file": file_alt}
+            count += 1
 
 with open("transfer.json", "w") as f:
     json.dump(result, f, indent=4)
